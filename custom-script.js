68481854da4a1d24f9ccb253d07f59d285a86d08
Yujinne <2291011@hansung.ac.kr>
2023-10-08 19:45:31 +0900

    homepage

--- custom-script.js
+++ custom-script.js
@@ -16,5 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     imageElement.addEventListener("mouseout", function() {
         imageElement.src = originalImageSrc;
+        
     });
 });
